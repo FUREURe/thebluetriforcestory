741403331c412d4e5a3985fd1c00bb09597062e4
FUREURe <fureure68@gmail.com>
2021-12-15 19:53:49 +0100

    maj de traduction anglaise
    ajout de traduction de quêtes

--- data/maps/Tricytown/first_map.lua
+++ data/maps/Tricytown/first_map.lua
@@ -12,7 +12,7 @@ local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
-  game:start_dialog('start.histoire')
+  game:start_dialog('story.main.1')
   -- You can initialize the movement and sprites of various
   -- map entities here.
 end
